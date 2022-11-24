c3a9dc1fd8a55ac19259c85219ba025f7f034ff6
Eric Devine <devineej@gmail.com>
2022-11-24 09:55:11 -0500

    serve an html file

--- demo/server/server.ts
+++ demo/server/server.ts
@@ -1,11 +1,13 @@
 import Koa from 'koa';
+import serve from 'koa-static';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const staticPath = path.join(dirname, '../static');
 
 const app = new Koa();
-
 const PORT = process.env.PORT || 8080;
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
-
+app.use(serve(staticPath));
 app.listen(PORT);
